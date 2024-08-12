3610ea891beca9e850f96217a2316f4bbea0b586
hazratbilal3537 <ba4407430@gmail.com>
2024-08-12 16:11:24 +0500

    First commit of portfolio

---
+++ js/portfolios.js
@@ -0,0 +1,142 @@
+var portfolio_data = [
+	{
+		id: 'ai-cover-project',
+		images: [
+			{
+				src: './images/ai-cover/1.png',
+				thumb: './images/ai-cover/1.png',
+				// subHtml: '<h4>Landing Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/ai-cover/2.png',
+				thumb: './images/ai-cover/2.png',
+				// subHtml: '<h4>Pizza Section</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/ai-cover/3.png',
+				thumb: './images/ai-cover/3.png',
+				// subHtml:
+				// 	'<h4>Side & Beverages Section</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/ai-cover/4.png',
+				thumb: './images/ai-cover/4.png',
+				// subHtml: '<h4>Cart Page</h4><p>Pizza Food WebApp Project</p>',
+			},
+			{
+				src: './images/ai-cover/5.png',
+				thumb: './images/ai-cover/5.png',
+				// subHtml: '<h4>Orders Page</h4><p>Pizza Food WebApp Project</p>',
+			}
+		],
+	},
+	{
+		id: 'shigoo-app-project',
+		images: [
+			{
+				src: './images/shigoo-app/1.png',
+				thumb: './images/shigoo-app/1.png',
+				// subHtml:
+				// 	'<h4>Dashboard Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/shigoo-app/2.png',
+				thumb: './images/shigoo-app/2.png',
+				// subHtml:
+				// 	'<h4>Search Feature</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/shigoo-app/3.png',
+				thumb: './images/shigoo-app/3.png',
+				// subHtml:
+				// 	'<h4>Transaction Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/shigoo-app/4.png',
+				thumb: './images/shigoo-app/4.png',
+				// subHtml:
+				// 	'<h4>Profile Page</h4><p>Expense Tracker Management Project</p>',
+			},
+			{
+				src: './images/shigoo-app/5.png',
+				thumb: './images/shigoo-app/5.png',
+				// subHtml:
+				// 	'<h4>Wallet Page with Dark Mode</h4><p>Expense Tracker Management Project</p>',
+			}
+		],
+	},
+	{
+		id: 'travel-plan-project',
+		images: [
+			{
+				src: './images/travel-plan/1.png',
+				thumb: './images/travel-plan/2.png',
+				// subHtml: '<h4>Landing Page</h4><p>Prajs NGO Project</p>',
+			},
+			{
+				src: './images/travel-plan/2.png',
+				thumb: './images/travel-plan/2.png',
+				// subHtml: '<h4>Recent Causes Section</h4><p>Prajs NGO Project</p>',
+			},
+			{
+				src: './images/travel-plan/3.png',
+				thumb: './images/travel-plan/3.png',
+			},
+			{
+				src: './images/travel-plan/4.png',
+				thumb: './images/travel-plan/4.png',
+			},
+			{
+				src: './images/travel-plan/5.png',
+				thumb: './images/travel-plan/5.png',
+			},
+			{
+				src: './images/travel-plan/06.png',
+				thumb: './images/travel-plan/06.png',
+			}
+		],
+	},
+	{
+		id: 'techtrack-advisor-project',
+		images: [
+			{
+				src: './images/techtrack-advisor/1.png',
+				thumb: './images/techtrack-advisor/1.png',
+				// subHtml:
+				// 	'<h4>Landing Page</h4><p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/techtrack-advisor/2.png',
+				thumb: './images/techtrack-advisor/2.png',
+				// subHtml:
+				// 	'<h4>Loader Page</h4><p>Splice Awwwards Music Band Web Design</p>',
+			},
+
+			{
+				src: './images/techtrack-advisor/3.png',
+				thumb: './images/techtrack-advisor/3.png',
+				// subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/techtrack-advisor/4.png',
+				thumb: './images/techtrack-advisor/4.png',
+				// subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/techtrack-advisor/5.png',
+				thumb: './images/techtrack-advisor/5.png',
+				// subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/techtrack-advisor/06.png',
+				thumb: './images/techtrack-advisor/06.png',
+				// subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			},
+			{
+				src: './images/techtrack-advisor/07.png',
+				thumb: './images/techtrack-advisor/07.png',
+				// subHtml: '<p>Splice Awwwards Music Band Web Design</p>',
+			}
+		],
+	}
+];
